b3bc2ee724449bc5b5bfbb2e60aa375b5929294e
Risa Yamamoto <yamari2422@gmail.com>
2024-08-08 16:21:45 -0700

    added : the function getting genre list for the opsion

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
 const body = document.querySelector("body");
 const screenModeButton = document.querySelectorAll('input[type="radio"]');
+const genreList = document.querySelector("genre");
+const form = document.querySelector("form");
+const showType = document.querySelector(".showType");
+const genre = document.querySelector(".genre");
 
+genre.addEventListener("click", (e) => {
+  fetchData();
+});
+
+// get genre list
+async function fetchData() {
+  let response;
+  genre.innerHTML = "";
+
+  if (showType.value === "all") {
+    // just for now
+    response = await getGenreList();
+  } else if (showType.value === "movies") {
+    // get a movie list
+    response = await getGenreList("movie");
+  } else {
+    // get a TV list
+    response = await getGenreList("tv");
+  }
+
+  // set the data as options
+  response.genres.forEach((data) => {
+    const element = document.createElement("option");
+    element.append(data.name);
+    genre.append(element);
+  });
+}
+
+// Toggle between light mode and dark mode
 screenModeButton.forEach((button) => {
   button.addEventListener("click", () => {
     if (button.classList.contains("dark-mode")) {
@@ -10,3 +43,27 @@ screenModeButton.forEach((button) => {
     }
   });
 });
+
+// get a genre list
+async function getGenreList(showType) {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNDhhMDdiNzMxNjI0NmQxMmYyNDUwZmU1NjU1OWEyNSIsIm5iZiI6MTcyMzEzNTg0NC4wOTYwMjYsInN1YiI6IjY2YjNiNzQyMDFlZjcyMTgzMjg4NmM0ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wc-OqAOtpMotV1UABiNEA2U77iZ3oIIlfykK0ReJJWQ",
+    },
+  };
+
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/genre/${showType}/list?language=en`,
+      options
+    );
+
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+}
